3b6fe9e417fbc6a85aa52b1ca77e48e4116e3860
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-06-22 22:16:00 -0400

    Update JS
    *JS*
    
    - added questions and answers arrays
    - added function to remove elements
    - added function to generate questions.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,15 @@ var myQuizStart = document.querySelector("#quiz-start");
 var myQuestion = document.querySelector("#display-question");
 var myQuizEnd = document.querySelector("#quiz-end");
 var myHighScore = document.querySelector("#highscore");
-console.log(myQuizStart);
+
+//Stored Questions
+var myQuestions = [
+    "How do you print to the console?", 
+    "A boolean is a variable that: "];
+
+var myAnswers = [
+    {A:"console.log", B:"print:", C:"console.print", D:"print.console"},
+    {A: "Returns True or False", B:"Returns a random number", C:"Returns the length of an array", D:"Returns an image size"}];
 
 //Functions
 function addToElement(myElementType, myElementToAppend, myText){
@@ -13,10 +21,32 @@ function addToElement(myElementType, myElementToAppend, myText){
     return;
 }
 
-function startQuiz(){
+function removeElements(myElementToClear){
+    var numElements = myElementToClear.children.length;
+    for(var i = 0; i < numElements; i++){
+        myElementToClear.children[0].remove();
+    }
+}
+
+function createQuiz(){
     addToElement("h1", myQuizStart, "Alvin's Javascript Quiz!");
     addToElement("p", myQuizStart, "Try to answer code related questions within the time limit!");
     addToElement("button", myQuizStart, "Start!");
+    myQuizStart.lastElementChild.setAttribute('onclick', 'generateQuestion(0)');
+    return;
 }
 
-startQuiz();
+function generateQuestion(myQAIndex){
+    removeElements(myQuizStart);
+    addToElement("h1", myQuizStart, myQuestions[myQAIndex]);
+    var answersArray = Object.values(myAnswers[myQAIndex]);
+    console.log(answersArray);
+    for(var i = 0; i<4; i++){
+        addToElement("button", myQuizStart, answersArray[i]);
+}
+return;
+}
+
+// Setup main section
+createQuiz();
+
